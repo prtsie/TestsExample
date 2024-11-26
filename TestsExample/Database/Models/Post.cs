208576ae890fc3d55aabee718708833eacac15d0
prtsie <157487565+prtsie@users.noreply.github.com>
2024-11-26 13:42:41 +0300

    Написал некоторые модели для БД

---
+++ TestsExample/Database/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace TestsExample.Database.Models
+{
+    /// <summary> Пост </summary>
+    public class Post
+    {
+        /// <summary> Идентификатор </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> Заголовок </summary>
+        public string Header { get; set; } = null!;
+
+        /// <summary> Текст поста </summary>
+        public string Body { get; set; } = null!;
+    }
+}
---
+++ TestsExample/Database/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TestsExample.Database.Models
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary> Идентификатор пользователя </summary>
+        public Guid Id { get; set; }
+
+        /// <summary> Имя пользователя </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary> Пароль пользователя </summary>
+        public string Password { get; set; } = null!;
+    }
+}
