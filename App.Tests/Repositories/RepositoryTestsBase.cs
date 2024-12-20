86a8a75c811305d5ef6b8d62866c4a7434e78d6e
prtsie <yonhsup@mail.ru>
2024-12-20 20:28:11 +0300

    Добавил комментарии и немного поменял базовый класс, где БД создается в памяти

--- App.Tests/Repositories/RepositoryTestsBase.cs
+++ App.Tests/Repositories/RepositoryTestsBase.cs
@@ -4,18 +4,24 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Repositories.Tests.Repositories;
 
+/// <summary>
+/// Базовый класс для тестов на репозитории
+/// </summary>
 public class RepositoryTestsBase
 {
+    /// <summary>
+    /// Контекст БД в оперативной памяти для тестирования 
+    /// </summary>
     protected readonly TestsSampleDbContext Context;
     
     public RepositoryTestsBase()
     {
         var opts = new DbContextOptionsBuilder<TestsSampleDbContext>()
-            .UseInMemoryDatabase("TestDB")
+            // Добавляем к названию БД случайный идентификатор, чтобы при параллельном выполнении тесты не мешали друг другу
+            .UseInMemoryDatabase($"TestDB{Guid.NewGuid()}")
+            // БД на памяти не поддерживает транзакции, так что можно отключить предупреждение
             .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
             .Options;
         Context = new(opts);
-        Context.Database.EnsureDeleted();
-        Context.Database.EnsureCreated();
     }
 }
